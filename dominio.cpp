a03f45d3dbc945d5a8966a3b7d208cc999217a75
guilodron <guilodron7@gmail.com>
2019-09-02 10:35:17 -0300

    Adiciona Classes
    Adiciona classes Assento e Bagagem com as implementações de seus metodos

--- dominio.cpp
+++ dominio.cpp
@@ -2,13 +2,51 @@
 #include <iostream>
 #include <cctype>
 
-/*Assento::Assento(char tipo){
-    std::toupper(tipo);
+
+
+Assento::Assento(char tipo){
+    set_tipo_de_assento(tipo);
+}
+
+char Assento::get_tipo_de_assento(){
+    return this->tipo_de_assento;
+}
+
+void Assento::set_tipo_de_assento(char tipo){
+    tipo = toupper(tipo);
     std::cout << tipo << std::endl;
     if(tipo == 'D' || tipo == 'T'){
         this->tipo_de_assento = tipo;
     }else {
         std::cout << "Tipo Inválido!!!" << std::endl;
     }
-}*/
+}
+
+////////////////////////////////////////////////////////
+
+void Bagagem::set_numero_de_bagagem(int num) {
+    if(num > 0 && num <= 4){
+        this->numero_de_bagagem = num;
+    }else {
+        std::cout << "Numero de Bagagem Inválido!!" << std::endl;
+    }
+}
+
+int Bagagem::get_numero_de_bagagem() {
+    return this->numero_de_bagagem;
+}
+
+Bagagem::Bagagem(int num) {
+    set_numero_de_bagagem(num);
+}
+
+//////////////////////////////////////////////////////
+
+
+
+
+
+
+
+
 
--- dominio.hpp
+++ dominio.hpp
@@ -1,12 +1,31 @@
 #ifndef DOMINIO_HPP
 #define DOMINIO_HPP
 
-/*class Assento{
+
+//////////////////////////////////////////
+//  Dominio Assento: recebe um char     //
+//  indicando o tipo de assento, sendo  //
+//  T = traseiro ou D = dianteiro       //
+//////////////////////////////////////////
+class Assento{
 private:
+    void set_tipo_de_assento(char tipo);
+    char get_tipo_de_assento();
     char tipo_de_assento;
 public:
     Assento(char tipo);
-};*/
+
+};
+
+class Bagagem{
+private:
+    int numero_de_bagagem;
+    void set_numero_de_bagagem(int num);
+    int get_numero_de_bagagem();
+public:
+    Bagagem(int num);
+};
+
 
 
 
