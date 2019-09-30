51a27ea55078880bbe72fb74c058995522b4c360
Marcos Blandim Andrade <marcos.andrade@latitude.unb.br>
2019-09-30 19:15:35 -0300

    Declaração da entidade usuário.

--- include/entidade.hpp
+++ include/entidade.hpp
@@ -1,6 +1,16 @@
 #ifndef ENTIDADE_HPP
 #define ENTIDADE_HPP
 
-
+// Nome, telefone, email, senha, CPF.
+class Usuario{
+private:
+    Nome nome = Nome(nome);
+    Telefone telefone = Telefone(telefone);
+    Email email = Email(email);
+    Senha senha = Senha(senha);
+    Cpf cpf = Cpf(cpf);
+public:
+    Usuario(Nome nome, Telefone telefone, Email email, Senha senha, Cpf cpf);
+};
 
 #endif
