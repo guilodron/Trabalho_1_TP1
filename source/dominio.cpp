77646c3a1576a7d9f68adbbca7466691bfb5c721
Marcos Blandim Andrade <marcos.andrade@latitude.unb.br>
2019-09-30 16:13:38 -0300

    Correção em erro do commit anterior.

--- source/dominio.cpp
+++ source/dominio.cpp
@@ -494,6 +494,7 @@ void Estado::valida_estado(std::string estado){
     std::string estados("AC-AL-AP-AM-BA-CE-DF-ES-GO-MA-MT-MS-MG-PA-PB-PR-PE-PI-RJ-RN-RS-RO-RR-SC-SP-SE-TO");
     if(!(estados.find(estado)!= std::string::npos && estado.find('-') == std::string::npos && estado.length() == 2)){
         throw std::invalid_argument("Sigla de Estado Inválida!!!");
+    }
 }
 
 /// @brief retorna o valor contido em estado
