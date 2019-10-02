a0634733609740194aeb7e5e78a7819afa826f34
guilodron <guilodron7@gmail.com>
2019-10-01 23:38:07 -0300

    Ajustes finais no teste de entidade

--- include/teste_entidade.hpp
+++ include/teste_entidade.hpp
@@ -7,19 +7,19 @@ using namespace std;
 
 class TUUsuario {
 private:
-// Defini��es de constantes para evitar numeros m�gicos.
-    const static std::string VALOR_VALIDO_NOME = "Marcos";
-    const static std::string VALOR_INVALIDO_NOME = "ABC!!@";
-	const static std::string VALOR_VALIDO_TELEFONE = "11-11-11111111";
-    const static std::string VALOR_INVALIDO_TELEFONE = "123";
-	const static std::string VALOR_VALIDO_EMAIL = "joao@email.com";
-    const static std::string VALOR_INVALIDO_EMAIL = ".joao.@..com";
-	const static std::string VALOR_VALIDO_SENHA = "#la$b";
-    const static std::string VALOR_INVALIDO_SENHA = "12345";
-	const static std::string VALOR_VALIDO_CPF = "034.869.281-12";
-    const static std::string VALOR_INVALIDO_CPF = "123.456.789-111";
+	// Defini��es de constantes para evitar numeros m�gicos.
+	const std::string VALOR_VALIDO_NOME = "Marcos";
+	const std::string VALOR_INVALIDO_NOME = "ABC!!@";
+	const std::string VALOR_VALIDO_TELEFONE = "11-11-111111111";
+	const std::string VALOR_INVALIDO_TELEFONE = "123";
+	const std::string VALOR_VALIDO_EMAIL = "joao@email.com";
+	const std::string VALOR_INVALIDO_EMAIL = ".joao.@..com";
+	const std::string VALOR_VALIDO_SENHA = "#la$b";
+	const std::string VALOR_INVALIDO_SENHA = "12345";
+	const std::string VALOR_VALIDO_CPF = "034.869.281-12";
+	const std::string VALOR_INVALIDO_CPF = "123.456.789-111";
 	// Refer�ncia para o objeto a ser testado.
-    Usuario* usuario;
+	Usuario* usuario;
 	// Estado do teste.
 	int estado;
 	// Declara��es de m�todos.
@@ -29,22 +29,22 @@ private:
 	void testar_cenario_falha();
 public:
 	// Defini��es de constantes para reportar resultado do teste.
-	const static int SUCESSO = 1;
-	const static int FALHA = 0;
+	const  int SUCESSO = 1;
+	const  int FALHA = 0;
 	int run();
 };
 
 class TUReserva {
 private:
-// Defini��es de constantes para evitar numeros m�gicos.
-    const static std::string VALOR_VALIDO_CODIGO = "12345";
-    const static std::string VALOR_INVALIDO_CODIGO = "123AB";
-	const static char VALOR_VALIDO_ASSENTO = "D";
-    const static char VALOR_INVALIDO_ASSENTO = "Z";
-	const static int VALOR_VALIDO_BAGAGEM = 4;
-    const static int VALOR_INVALIDO_BAGAGEM = 5;
+	// Defini��es de constantes para evitar numeros m�gicos.
+	const std::string VALOR_VALIDO_CODIGO = "12345";
+	const std::string VALOR_INVALIDO_CODIGO = "123AB";
+	const char VALOR_VALIDO_ASSENTO = 'D';
+	const char VALOR_INVALIDO_ASSENTO = 'Z';
+	const int VALOR_VALIDO_BAGAGEM = 4;
+	const int VALOR_INVALIDO_BAGAGEM = 5;
 	// Refer�ncia para o objeto a ser testado.
-    Reserva* reserva;
+	Reserva* reserva;
 	// Estado do teste.
 	int estado;
 	// Declara��es de m�todos.
@@ -54,34 +54,34 @@ private:
 	void testar_cenario_falha();
 public:
 	// Defini��es de constantes para reportar resultado do teste.
-	const static int SUCESSO = 1;
-	const static int FALHA = 0;
+	const  int SUCESSO = 1;
+	const  int FALHA = 0;
 	int run();
 };
 
 class TUCarona {
 private:
-// Defini��es de constantes para evitar numeros m�gicos.
-    const static char VALOR_VALIDO_CODIGO = "1234";
-    const static char VALOR_INVALIDO_CODIGO = "123x";\
-	const static char VALOR_VALIDO_CIDADE_ORIGEM = "Brasilia";
-    const static char VALOR_INVALIDO_CIDADE_ORIGEM = "Bras  ilia";
-	const static char VALOR_VALIDO_ESTADO_ORIGEM = "Sao paulo";
-    const static char VALOR_INVALIDO_ESTADO_ORIGEM = "Sao  paulo";
-	const static char VALOR_VALIDO_CIDADE_DESTINO = "Ilheus";
-    const static char VALOR_INVALIDO_CIDADE_DESTINO = "Ilh  eus";
-	const static char VALOR_VALIDO_ESTADO_DESTINO = "Rio";
-    const static char VALOR_INVALIDO_ESTADO_DESTINO = "R  io";
-	const static char VALOR_VALIDO_DATA = "22/11/2001";
-    const static char VALOR_INVALIDO_DATA = "31/02/2004";
-	const static int VALOR_VALIDO_DURACAO = 10;
-    const static int VALOR_INVALIDO_DURACAO = 500;
-	const static int VALOR_VALIDO_VAGAS = 3;
-    const static int VALOR_INVALIDO_VAGAS = 10;
-	const static float VALOR_VALIDO_PRECO = 50.00;
-    const static float VALOR_INVALIDO_PRECO = 6000;
+	// Defini��es de constantes para evitar numeros m�gicos.
+	const std::string VALOR_VALIDO_CODIGO = "1234";
+	const std::string VALOR_INVALIDO_CODIGO = "123x"; \
+		const std::string VALOR_VALIDO_CIDADE_ORIGEM = "Brasilia";
+	const std::string VALOR_INVALIDO_CIDADE_ORIGEM = "Bras  ilia";
+	const std::string VALOR_VALIDO_ESTADO_ORIGEM = "SP";
+	const std::string VALOR_INVALIDO_ESTADO_ORIGEM = "XX";
+	const std::string VALOR_VALIDO_CIDADE_DESTINO = "Ilheus";
+	const std::string VALOR_INVALIDO_CIDADE_DESTINO = "Ilh  eus";
+	const std::string VALOR_VALIDO_ESTADO_DESTINO = "RJ";
+	const std::string VALOR_INVALIDO_ESTADO_DESTINO = "LL";
+	const std::string VALOR_VALIDO_DATA = "22/11/2001";
+	const std::string VALOR_INVALIDO_DATA = "31/02/2004";
+	const int VALOR_VALIDO_DURACAO = 10;
+	const int VALOR_INVALIDO_DURACAO = 500;
+	const int VALOR_VALIDO_VAGAS = 3;
+	const int VALOR_INVALIDO_VAGAS = 10;
+	const float VALOR_VALIDO_PRECO = 50.00;
+	const float VALOR_INVALIDO_PRECO = 6000;
 	// Refer�ncia para o objeto a ser testado.
-    Carona* carona;
+	Carona* carona;
 	// Estado do teste.
 	int estado;
 	// Declara��es de m�todos.
@@ -91,22 +91,22 @@ private:
 	void testar_cenario_falha();
 public:
 	// Defini��es de constantes para reportar resultado do teste.
-	const static int SUCESSO = 1;
-	const static int FALHA = 0;
+	const  int SUCESSO = 1;
+	const  int FALHA = 0;
 	int run();
 };
 
 class TUConta {
 private:
-// Defini��es de constantes para evitar numeros m�gicos.
-    const std::string VALOR_VALIDO_CODIGO = "123";
+	// Defini��es de constantes para evitar numeros m�gicos.
+	const std::string VALOR_VALIDO_CODIGO = "123";
 	const std::string VALOR_INVALIDO_CODIGO = "12";
 	const std::string VALOR_VALIDO_AGENCIA = "6938-5";
 	const std::string VALOR_INVALIDO_AGENCIA = "6938-4";
 	const std::string VALOR_VALIDO_CONTA = "452765-1";
 	const std::string VALOR_INVALIDO_CONTA = "452765-2";
 	// Refer�ncia para o objeto a ser testado.
-    Conta* conta;
+	Conta* conta;
 	// Estado do teste.
 	int estado;
 	// Declara��es de m�todos.
@@ -116,7 +116,9 @@ private:
 	void testar_cenario_falha();
 public:
 	// Defini��es de constantes para reportar resultado do teste.
-	const static int SUCESSO = 1;
-	const static int FALHA = 0;
+	const  int SUCESSO = 1;
+	const  int FALHA = 0;
 	int run();
 };
+
+#endif
--- source/teste_entidade.cpp
+++ source/teste_entidade.cpp
@@ -1,5 +1,5 @@
 #include "../include/teste_entidade.hpp"
-#include <stdexcept>
+#include <exception>
 #include <iostream>
 
 void TUUsuario::set_up()
@@ -27,7 +27,7 @@ void TUUsuario::testar_cenario_sucesso()
 			cout << "Passou no cenario de sucesso de nome" << std::endl;
 		}
 
-        usuario->set_telefone(VALOR_VALIDO_TELEFONE);
+		usuario->set_telefone(VALOR_VALIDO_TELEFONE);
 		if (usuario->get_telefone() != VALOR_VALIDO_TELEFONE) {
 			std::cout << "Falha no get telefone cenario de sucesso!" << std::endl;
 			estado = FALHA;
@@ -36,7 +36,7 @@ void TUUsuario::testar_cenario_sucesso()
 			cout << "Passou no cenario de sucesso de telefone" << std::endl;
 		}
 
-        usuario->set_email(VALOR_VALIDO_EMAIL);
+		usuario->set_email(VALOR_VALIDO_EMAIL);
 		if (usuario->get_email() != VALOR_VALIDO_EMAIL) {
 			std::cout << "Falha no get email cenario de sucesso!" << std::endl;
 			estado = FALHA;
@@ -45,7 +45,7 @@ void TUUsuario::testar_cenario_sucesso()
 			cout << "Passou no cenario de sucesso de email" << std::endl;
 		}
 
-        usuario->set_senha(VALOR_VALIDO_SENHA);
+		usuario->set_senha(VALOR_VALIDO_SENHA);
 		if (usuario->get_senha() != VALOR_VALIDO_SENHA) {
 			std::cout << "Falha no get senha cenario de sucesso!" << std::endl;
 			estado = FALHA;
@@ -54,7 +54,7 @@ void TUUsuario::testar_cenario_sucesso()
 			cout << "Passou no cenario de sucesso de senha" << std::endl;
 		}
 
-        usuario->set_cpf(VALOR_VALIDO_CPF);
+		usuario->set_cpf(VALOR_VALIDO_CPF);
 		if (usuario->get_cpf() != VALOR_VALIDO_CPF) {
 			std::cout << "Falha no get cpf cenario de sucesso!" << std::endl;
 			estado = FALHA;
@@ -63,7 +63,7 @@ void TUUsuario::testar_cenario_sucesso()
 			cout << "Passou no cenario de sucesso de cpf" << std::endl;
 		}
 	}
-	catch (std::invalid_argument e) {
+	catch (std::invalid_argument& e) {
 		std::cout << "Falha no set!" << std::endl;
 	}
 }
@@ -74,15 +74,15 @@ void TUUsuario::testar_cenario_falha()
 	std::cout << "CENARIO DE FALHA" << std::endl;
 	try {
 		usuario->set_nome(VALOR_INVALIDO_NOME);
-        usuario->set_telefone(VALOR_INVALIDO_TELEFONE);
-        usuario->set_email(VALOR_INVALIDO_EMAIL);
-        usuario->set_senha(VALOR_INVALIDO_SENHA);
-        usuario->set_cpf(VALOR_INVALIDO_CPF);
+		usuario->set_telefone(VALOR_INVALIDO_TELEFONE);
+		usuario->set_email(VALOR_INVALIDO_EMAIL);
+		usuario->set_senha(VALOR_INVALIDO_SENHA);
+		usuario->set_cpf(VALOR_INVALIDO_CPF);
 		std::cout << "Falha no cenario invalido!" << std::endl;
 		estado = FALHA;
 
 	}
-	catch(std::invalid_argument e){
+	catch (std::invalid_argument& e) {
 		std::cout << "Passou no cenario invalido" << std::endl;
 	}
 }
@@ -125,7 +125,7 @@ void TUReserva::testar_cenario_sucesso()
 			cout << "Passou no cenario de sucesso de codigo" << std::endl;
 		}
 
-        reserva->set_assento(VALOR_VALIDO_ASSENTO);
+		reserva->set_assento(VALOR_VALIDO_ASSENTO);
 		if (reserva->get_assento() != VALOR_VALIDO_ASSENTO) {
 			std::cout << "Falha no get assento cenario de sucesso!" << std::endl;
 			estado = FALHA;
@@ -133,8 +133,8 @@ void TUReserva::testar_cenario_sucesso()
 		else {
 			cout << "Passou no cenario de sucesso de assento" << std::endl;
 		}
-        
-        reserva->set_bagagem(VALOR_VALIDO_BAGAGEM);
+
+		reserva->set_bagagem(VALOR_VALIDO_BAGAGEM);
 		if (reserva->get_bagagem() != VALOR_VALIDO_BAGAGEM) {
 			std::cout << "Falha no get bagagem cenario de sucesso!" << std::endl;
 			estado = FALHA;
@@ -144,7 +144,7 @@ void TUReserva::testar_cenario_sucesso()
 		}
 
 	}
-	catch (std::invalid_argument e) {
+	catch (std::invalid_argument& e) {
 		std::cout << "Falha no set!" << std::endl;
 	}
 }
@@ -155,13 +155,13 @@ void TUReserva::testar_cenario_falha()
 	std::cout << "CENARIO DE FALHA" << std::endl;
 	try {
 		reserva->set_codigo_de_reserva(VALOR_INVALIDO_CODIGO);
-        reserva->set_assento(VALOR_INVALIDO_ASSENTO);
-        reserva->set_bagagem(VALOR_INVALIDO_BAGAGEM);
+		reserva->set_assento(VALOR_INVALIDO_ASSENTO);
+		reserva->set_bagagem(VALOR_INVALIDO_BAGAGEM);
 		std::cout << "Falha no cenario invalido!" << std::endl;
 		estado = FALHA;
 
 	}
-	catch(std::invalid_argument e){
+	catch (std::invalid_argument& e) {
 		std::cout << "Passou no cenario invalido" << std::endl;
 	}
 }
@@ -204,7 +204,7 @@ void TUCarona::testar_cenario_sucesso()
 			cout << "Passou no cenario de sucesso de codigo" << std::endl;
 		}
 
-        carona->set_cidade_de_origem(VALOR_VALIDO_CIDADE_ORIGEM);
+		carona->set_cidade_de_origem(VALOR_VALIDO_CIDADE_ORIGEM);
 		if (carona->get_cidade_de_origem() != VALOR_VALIDO_CIDADE_ORIGEM) {
 			std::cout << "Falha no get cidade origem cenario de sucesso!" << std::endl;
 			estado = FALHA;
@@ -213,7 +213,7 @@ void TUCarona::testar_cenario_sucesso()
 			cout << "Passou no cenario de sucesso de cidade origem" << std::endl;
 		}
 
-        carona->set_estado_de_origem(VALOR_VALIDO_ESTADO_ORIGEM);
+		carona->set_estado_de_origem(VALOR_VALIDO_ESTADO_ORIGEM);
 		if (carona->get_estado_de_origem() != VALOR_VALIDO_ESTADO_ORIGEM) {
 			std::cout << "Falha no get estado origem cenario de sucesso!" << std::endl;
 			estado = FALHA;
@@ -222,7 +222,7 @@ void TUCarona::testar_cenario_sucesso()
 			cout << "Passou no cenario de sucesso de estado origem" << std::endl;
 		}
 
-        carona->set_cidade_de_destino(VALOR_VALIDO_CIDADE_DESTINO);
+		carona->set_cidade_de_destino(VALOR_VALIDO_CIDADE_DESTINO);
 		if (carona->get_cidade_de_destino() != VALOR_VALIDO_CIDADE_DESTINO) {
 			std::cout << "Falha no get cidade destino cenario de sucesso!" << std::endl;
 			estado = FALHA;
@@ -231,7 +231,7 @@ void TUCarona::testar_cenario_sucesso()
 			cout << "Passou no cenario de sucesso de cidade destino" << std::endl;
 		}
 
-        carona->set_estado_de_destino(VALOR_VALIDO_ESTADO_DESTINO);
+		carona->set_estado_de_destino(VALOR_VALIDO_ESTADO_DESTINO);
 		if (carona->get_estado_de_destino() != VALOR_VALIDO_ESTADO_DESTINO) {
 			std::cout << "Falha no get estado destino cenario de sucesso!" << std::endl;
 			estado = FALHA;
@@ -240,7 +240,7 @@ void TUCarona::testar_cenario_sucesso()
 			cout << "Passou no cenario de sucesso de estado destino" << std::endl;
 		}
 
-        carona->set_data(VALOR_VALIDO_DATA);
+		carona->set_data(VALOR_VALIDO_DATA);
 		if (carona->get_data() != VALOR_VALIDO_DATA) {
 			std::cout << "Falha no get data cenario de sucesso!" << std::endl;
 			estado = FALHA;
@@ -249,7 +249,7 @@ void TUCarona::testar_cenario_sucesso()
 			cout << "Passou no cenario de sucesso de data" << std::endl;
 		}
 
-        carona->set_duracao(VALOR_VALIDO_DURACAO);
+		carona->set_duracao(VALOR_VALIDO_DURACAO);
 		if (carona->get_duracao() != VALOR_VALIDO_DURACAO) {
 			std::cout << "Falha no get duracao cenario de sucesso!" << std::endl;
 			estado = FALHA;
@@ -258,7 +258,7 @@ void TUCarona::testar_cenario_sucesso()
 			cout << "Passou no cenario de sucesso de duracao" << std::endl;
 		}
 
-        carona->set_vagas(VALOR_VALIDO_VAGAS);
+		carona->set_vagas(VALOR_VALIDO_VAGAS);
 		if (carona->get_vagas() != VALOR_VALIDO_VAGAS) {
 			std::cout << "Falha no get vagas cenario de sucesso!" << std::endl;
 			estado = FALHA;
@@ -267,7 +267,7 @@ void TUCarona::testar_cenario_sucesso()
 			cout << "Passou no cenario de sucesso de vagas" << std::endl;
 		}
 
-        carona->set_preco(VALOR_VALIDO_PRECO);
+		carona->set_preco(VALOR_VALIDO_PRECO);
 		if (carona->get_preco() != VALOR_VALIDO_PRECO) {
 			std::cout << "Falha no get preco cenario de sucesso!" << std::endl;
 			estado = FALHA;
@@ -277,7 +277,7 @@ void TUCarona::testar_cenario_sucesso()
 		}
 
 	}
-	catch (std::invalid_argument e) {
+	catch (std::invalid_argument& e) {
 		std::cout << "Falha no set!" << std::endl;
 	}
 }
@@ -288,19 +288,19 @@ void TUCarona::testar_cenario_falha()
 	std::cout << "CENARIO DE FALHA" << std::endl;
 	try {
 		carona->set_codigo_de_carona(VALOR_INVALIDO_CODIGO);
-        carona->set_cidade_de_origem(VALOR_INVALIDO_CIDADE_ORIGEM);
-        carona->set_estado_de_origem(VALOR_INVALIDO_ESTADO_ORIGEM);
-        carona->set_cidade_de_destino(VALOR_INVALIDO_CIDADE_DESTINO);
-        carona->set_estado_de_destino(VALOR_INVALIDO_ESTADO_DESTINO);
-        carona->set_data(VALOR_INVALIDO_DATA);
-        carona->set_duracao(VALOR_INVALIDO_DURACAO);
-        carona->set_vagas(VALOR_INVALIDO_VAGAS);
-        carona->set_preco(VALOR_INVALIDO_PRECO);
+		carona->set_cidade_de_origem(VALOR_INVALIDO_CIDADE_ORIGEM);
+		carona->set_estado_de_origem(VALOR_INVALIDO_ESTADO_ORIGEM);
+		carona->set_cidade_de_destino(VALOR_INVALIDO_CIDADE_DESTINO);
+		carona->set_estado_de_destino(VALOR_INVALIDO_ESTADO_DESTINO);
+		carona->set_data(VALOR_INVALIDO_DATA);
+		carona->set_duracao(VALOR_INVALIDO_DURACAO);
+		carona->set_vagas(VALOR_INVALIDO_VAGAS);
+		carona->set_preco(VALOR_INVALIDO_PRECO);
 		std::cout << "Falha no cenario invalido!" << std::endl;
 		estado = FALHA;
 
 	}
-	catch(std::invalid_argument e){
+	catch (std::invalid_argument& e) {
 		std::cout << "Passou no cenario invalido" << std::endl;
 	}
 }
@@ -335,7 +335,7 @@ void TUConta::testar_cenario_sucesso()
 	std::cout << "CENARIO DE SUCESSO" << std::endl;
 	try {
 		conta->set_codigo_de_banco(VALOR_VALIDO_CODIGO);
-		if (conta->set_codigo_de_banco() != VALOR_VALIDO_CODIGO) {
+		if (conta->get_codigo_de_banco() != VALOR_VALIDO_CODIGO) {
 			std::cout << "Falha no get codigo cenario de sucesso!" << std::endl;
 			estado = FALHA;
 		}
@@ -343,17 +343,17 @@ void TUConta::testar_cenario_sucesso()
 			cout << "Passou no cenario de sucesso de codigo" << std::endl;
 		}
 
-        conta->set_numero_de_agencia(VALOR_VALIDO_AGENCIA);
-		if (conta->set_numero_de_agencia() != VALOR_VALIDO_AGENCIA) {
+		conta->set_numero_de_agencia(VALOR_VALIDO_AGENCIA);
+		if (conta->get_numero_de_agencia() != VALOR_VALIDO_AGENCIA) {
 			std::cout << "Falha no get agencia cenario de sucesso!" << std::endl;
 			estado = FALHA;
 		}
 		else {
 			cout << "Passou no cenario de sucesso de agencia" << std::endl;
 		}
-        
-        conta->set_numero_de_conta(VALOR_VALIDO_CONTA);
-		if (conta->set_numero_de_conta() != VALOR_VALIDO_CONTA) {
+
+		conta->set_numero_de_conta(VALOR_VALIDO_CONTA);
+		if (conta->get_numero_de_conta() != VALOR_VALIDO_CONTA) {
 			std::cout << "Falha no get conta cenario de sucesso!" << std::endl;
 			estado = FALHA;
 		}
@@ -362,7 +362,7 @@ void TUConta::testar_cenario_sucesso()
 		}
 
 	}
-	catch (std::invalid_argument e) {
+	catch (std::invalid_argument& e) {
 		std::cout << "Falha no set!" << std::endl;
 	}
 }
@@ -373,13 +373,13 @@ void TUConta::testar_cenario_falha()
 	std::cout << "CENARIO DE FALHA" << std::endl;
 	try {
 		conta->set_codigo_de_banco(VALOR_INVALIDO_CODIGO);
-        conta->set_numero_de_agencia(VALOR_INVALIDO_AGENCIA);
-        conta->set_numero_de_conta(VALOR_INVALIDO_CONTA);
+		conta->set_numero_de_agencia(VALOR_INVALIDO_AGENCIA);
+		conta->set_numero_de_conta(VALOR_INVALIDO_CONTA);
 		std::cout << "Falha no cenario invalido!" << std::endl;
 		estado = FALHA;
 
 	}
-	catch(std::invalid_argument e){
+	catch (std::invalid_argument& e) {
 		std::cout << "Passou no cenario invalido" << std::endl;
 	}
 }
@@ -394,5 +394,3 @@ int TUConta::run()
 	std::cout << "--------------------------" << std::endl << std::endl;
 	return estado;
 }
-
-//---------------------------------------------------------------------------
